06d14fb7dae4a07844a418de7a5e77813bf31338
Robert Sun <sunwrobert@gmail.com>
2022-03-16 21:22:25 -0700

    Remove erroneous

--- hardhat.config.d.ts
+++
@@ -1,7 +0,0 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-declare const config: HardhatUserConfig;
-export default config;
