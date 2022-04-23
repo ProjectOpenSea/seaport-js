7c5197c6b65721fe38fdda28499a2b14d6cc8da7
Robert Sun <sunwrobert@gmail.com>
2022-04-23 15:47:31 +0800

    Fix approval amounts and dedupe in approval actions

--- src/__tests__/bundle.spec.ts
+++ src/__tests__/bundle.spec.ts
@@ -283,7 +283,7 @@ describeWithFixture(
           };
         });
 
-        it("ERC20 <=> ERC721", async () => {
+        it.only("ERC20 <=> ERC721", async () => {
           const { consideration, testErc721, testErc20 } = fixture;
 
           const { executeAllActions } = await consideration.createOrder(
