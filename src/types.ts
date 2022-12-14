f2530f954c673e0578bd206ecb6b0fc5fcb1b898
Martin Kainzbauer <m.kainzbauer@xtools.at>
2022-12-14 11:23:09 +0100

    fix: consolidate types

--- src/types.ts
+++ src/types.ts
@@ -113,24 +113,12 @@ export type BasicErc721Item = {
 export type Erc721ItemWithCriteria = {
   itemType: ItemType.ERC721;
   token: string;
-  identifiers: string[];
+  amount?: string;
+  endAmount?: string;
   // Used for criteria based items i.e. offering to buy 5 NFTs for a collection
-  amount?: string;
-  endAmount?: string;
-};
+} & ({ identifiers: string[] } | { criteria: string });
 
-export type Erc721ItemWithMerkleRoot = {
-  itemType: ItemType.ERC721;
-  token: string;
-  criteria: string;
-  amount?: string;
-  endAmount?: string;
-};
-
-type Erc721Item =
-  | BasicErc721Item
-  | Erc721ItemWithCriteria
-  | Erc721ItemWithMerkleRoot;
+type Erc721Item = BasicErc721Item | Erc721ItemWithCriteria;
 
 export type BasicErc1155Item = {
   itemType: ItemType.ERC1155;
@@ -143,23 +131,11 @@ export type BasicErc1155Item = {
 export type Erc1155ItemWithCriteria = {
   itemType: ItemType.ERC1155;
   token: string;
-  identifiers: string[];
   amount: string;
   endAmount?: string;
-};
+} & ({ identifiers: string[] } | { criteria: string });
 
-export type Erc1155ItemWithMerkleRoot = {
-  itemType: ItemType.ERC1155;
-  token: string;
-  criteria: string;
-  amount: string;
-  endAmount?: string;
-};
-
-type Erc1155Item =
-  | BasicErc1155Item
-  | Erc1155ItemWithCriteria
-  | Erc1155ItemWithMerkleRoot;
+type Erc1155Item = BasicErc1155Item | Erc1155ItemWithCriteria;
 
 export type CurrencyItem = {
   token?: string;
