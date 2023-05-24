617253f7ba2ae3f65264b05d6558815e2302f992
Ryan Ghods <ryan@ryanio.com>
2023-05-24 14:57:53 -0700

    update deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,13 +3,12 @@ import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
-import "hardhat-gas-reporter";
 import "solidity-coverage";
 
 dotenv.config();
 
-// Go to https://hardhat.org/config/ to learn more
-const config: HardhatUserConfig = {
+// Go to https://hardhat.org/config to learn more
+const config: HardhatUserConfig & { typechain: any } = {
   solidity: {
     compilers: [
       {
@@ -46,10 +45,6 @@ const config: HardhatUserConfig = {
       allowUnlimitedContractSize: true,
       chainId: 1,
     },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
   },
   typechain: {
     outDir: "src/typechain",
